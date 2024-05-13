f29d2d1f84c60971f4b7986296adf61b44716c10
Renato Ponce <rponcel@unsa.edu.pe>
2024-05-12 22:07:14 -0500

    Poniendo un mensaje inicial en el programa

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -6,6 +6,7 @@ public class PruebaGit {
     {
         int numero1,numero2,producto;
         
+        JOptionPane.showMessageDialog(null, "Vamos a ver que tal funciona este programa");
         numero1=Integer.parseInt(JOptionPane.showInputDialog("Ingresar número 1: "));
         numero2=Integer.parseInt(JOptionPane.showInputDialog("Ingresar número 2: "));
         producto=numero1*numero2;
