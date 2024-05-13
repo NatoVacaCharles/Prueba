98773e79efe9f4bab6ad887688ab338f4971f73c
Renato Ponce <rponcel@unsa.edu.pe>
2024-05-12 20:57:44 -0500

    Subiendo código

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pruebagit;
-
-/**
- *
- * @author RENATO
- */
+import javax.swing.JOptionPane;
 public class PruebaGit {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        int numero1,numero2,producto;
+        
+        numero1=Integer.parseInt(JOptionPane.showInputDialog("Ingresar número 1: "));
+        numero2=Integer.parseInt(JOptionPane.showInputDialog("Ingresar número 2: "));
+        producto=numero1*numero2;
+        JOptionPane.showMessageDialog(null, "El resultado es "+producto);
     }
     
 }
